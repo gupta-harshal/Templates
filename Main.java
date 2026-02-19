d46eed69315640ebb4e4126d3565271bb249de54
Harshal Gupta <143176417+gupta-harshal@users.noreply.github.com>
2026-02-19 14:11:54 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -44,41 +44,6 @@ public class Main {
         while (t-- > 0) {
 
             int n = jio.nextInt();
-            long[] cnt = new long[n + 1];
-
-            for (int i = 0; i < n; i++) {
-                int x = jio.nextInt();
-                cnt[x]++;
-            }
-
-            long ans = 0;
-
-            for (int i = 2; i < n; i++) {
-                ans += cnt[i - 1] * cnt[i] * cnt[i + 1];
-            }
-
-
-            for (int i = 1; i < n; i++) {
-                ans += (cnt[i] * (cnt[i] - 1) / 2) * cnt[i + 1];
-            }
-
-            for (int i = 2; i <= n; i++) {
-                ans += cnt[i - 1] * (cnt[i] * (cnt[i] - 1) / 2);
-            }
-
-            for (int i = 2; i < n; i++) {
-                ans += cnt[i - 1] * (cnt[i + 1] * (cnt[i + 1] - 1) / 2);
-            }
-
-            for (int i = 2; i < n; i++) {
-                ans += cnt[i + 1] * (cnt[i - 1] * (cnt[i - 1] - 1) / 2);
-            }
-
-            for (int i = 1; i <= n; i++) {
-                ans += cnt[i] * (cnt[i] - 1) * (cnt[i] - 2) / 6;
-            }
-
-            jio.println(ans);
         }
     }
 }
